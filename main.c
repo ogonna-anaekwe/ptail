44e8c20a17f11c9e4339d5a2796a9d787818e25f
ogonna-anaekwe <ogonna.anaekwe@gmail.com>
2022-07-29 23:16:20 -0400

    update readme

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
     if (argc < 2 || argc > 3)
     {
-        fprintf(stderr, "Usage: ./ptail <file_path> <optional: num_of_lines_to_print>\n");
+        fprintf(stderr, "usage: ./ptail <file_path> <optional: num_of_lines_to_print>\n");
         exit(1);
     }
 
